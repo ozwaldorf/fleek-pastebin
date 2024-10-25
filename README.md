d0e50ba86d82dc33f368369063eabd821d6f2c9b
ozwaldorf <self@ossian.dev>
2024-10-24 22:01:56 -0400

    genesis

---
+++ README.md
@@ -0,0 +1,19 @@
+# Fleek Pastebin
+
+Pastebin service for the Fleek Network javascript runtime.
+
+Backed by 0dd.sh, which enables constructing a temporary origin url in order
+to cache content in the node and propagate across the network.
+
+## Development
+
+```bash
+# Install fleek cli
+deno install -gA npm:@fleek-platform/cli
+
+# Initialize function
+deno task init
+
+# Deploy function
+deno task deploy
+```
---
+++ index.js
@@ -0,0 +1,109 @@
+import * as Blake3 from 'ipfs://bafkreidlknwg33twrtlm5tagbpdyhkovouzkpkp2sfpp4n2i6o4jiclq5i';
+import { Buffer } from 'node:buffer';
+
+// Hex < - > Bytes
+const htob = s => new Uint8Array([...s.matchAll(/../g)].map(m => parseInt(m[0], 16)));
+const btoh = b => [...new Uint8Array(b)].map((x) => x.toString(16).padStart(2, "0")).join("");
+
+const host = "paste.functions.on-fleek.app";
+
+export async function main({ path, method, body }) {
+  switch (method) {
+    case "GET": return await handleGet(path);
+    case "PUT": return await handlePut(body);
+  }
+}
+
+function handleGet(path) {
+  if (path == "" || path == "/") {
+    return handleUsage()
+  } else {
+    return handleContent(path)
+  }
+}
+
+function handleUsage() {
+  return `\
+PASTEBIN(1)             User Commands             PASTEBIN(1)
+
+ USAGE
+   View help    :  curl ${host} -L
+   Upload file  :  curl ${host} -LT file
+
+ DESCRIPTION
+   Command line pastebin on fleek network.
+
+   Pastes are identified by their hex-encoded blake3 hashes.
+   Currently backed by 0dd.sh for temporary storage to cache
+   data on the network.
+
+ SEE ALSO
+   * Fleek Network  :  https://fleek.network
+   * 0dd.sh         :  https://0dd.sh
+`;
+}
+
+
+async function handleContent(path) {
+  console.log(path);
+  // Parse hash from request path
+  const hash = path.split('/')[1];
+  console.log(hash);
+  if (!hash || hash.length !== 64)
+    return "not found";
+  const bytes = htob(hash);
+
+  // Sync paste from cache
+  await Fleek.fetchBlake3(bytes);
+
+  // Load handle to content and read all blocks
+  const handle = await Fleek.loadContent(new Uint8Array(bytes));
+  let data = [];
+  for (let i = 0; i < handle.length; i++) {
+    data.push(await handle.readBlock(i));
+  }
+
+  // Concat data together and stream
+  const flatArray = data.reduce((acc, curr) => {
+    acc.push(...curr);
+    return acc;
+  }, []);
+  return new Uint8Array(flatArray);
+}
+
+async function handlePut(body) {
+  // normalize body into byte array
+  let bytes;
+  if (ArrayBuffer.isView(body)) {
+    bytes = body;
+  } else {
+    const encoder = new TextEncoder();
+    switch (typeof body) {
+      case 'undefined':
+        return 'empty upload body';
+      case 'string':
+        bytes = encoder.encode(body);
+        break;
+      default:
+        const str = JSON.stringify(body);
+        bytes = encoder.encode(str);
+        break;
+    }
+  }
+
+  // Hash content
+  const digest = Blake3.hash(new Uint8Array(bytes));
+
+  // PUT request to 0dd.sh for temp storage and construct a sri hashed origin url
+  const res = await fetch("https://0dd.sh", { method: "PUT", body: bytes });
+  const text = await res.text();
+  if (res.status !== 200) {
+    return text;
+  }
+  const originUrl = `${text.trim()}#integrity=blake3-${Buffer.from(digest).toString('base64')}`;
+
+  // Introduce the computed origin url to the network, caching the blake3 data
+  await Fleek.fetchFromOrigin(originUrl);
+
+  return `${host}/${btoh(digest)}`
+}
